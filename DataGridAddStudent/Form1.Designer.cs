ac914208c16ade497825d2c4ae0c984a2d4a365e
Kris Jane Cabilan <cabilankrisjane@gmail.com>
2024-01-16 21:15:22 +0800

    Add project files.

---
+++ DataGridAddStudent/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace DataGridAddStudent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StudentsGrid = new System.Windows.Forms.DataGridView();
+            this.StudentName = new System.Windows.Forms.TextBox();
+            this.StudentAge = new System.Windows.Forms.TextBox();
+            this.isSingleCheckBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.pictureBoxQRCode = new System.Windows.Forms.PictureBox();
+            this.StudentID = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // StudentsGrid
+            // 
+            this.StudentsGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.StudentsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentsGrid.Location = new System.Drawing.Point(439, 84);
+            this.StudentsGrid.Name = "StudentsGrid";
+            this.StudentsGrid.RowHeadersWidth = 62;
+            this.StudentsGrid.Size = new System.Drawing.Size(574, 261);
+            this.StudentsGrid.TabIndex = 0;
+            this.StudentsGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            // 
+            // StudentName
+            // 
+            this.StudentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StudentName.Location = new System.Drawing.Point(225, 68);
+            this.StudentName.Multiline = true;
+            this.StudentName.Name = "StudentName";
+            this.StudentName.Size = new System.Drawing.Size(170, 32);
+            this.StudentName.TabIndex = 1;
+            this.StudentName.TextChanged += new System.EventHandler(this.StudentName_TextChanged);
+            // 
+            // StudentAge
+            // 
+            this.StudentAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.StudentAge.Location = new System.Drawing.Point(225, 122);
+            this.StudentAge.Multiline = true;
+            this.StudentAge.Name = "StudentAge";
+            this.StudentAge.Size = new System.Drawing.Size(170, 32);
+            this.StudentAge.TabIndex = 2;
+            this.StudentAge.TextChanged += new System.EventHandler(this.StudentAge_TextChanged);
+            // 
+            // isSingleCheckBox
+            // 
+            this.isSingleCheckBox.AutoSize = true;
+            this.isSingleCheckBox.Location = new System.Drawing.Point(225, 168);
+            this.isSingleCheckBox.Name = "isSingleCheckBox";
+            this.isSingleCheckBox.Size = new System.Drawing.Size(15, 14);
+            this.isSingleCheckBox.TabIndex = 3;
+            this.isSingleCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(104, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Student Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(104, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Age:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(104, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Is Single?";
+            // 
+            // BtnUpdate
+            // 
+            this.BtnUpdate.BackColor = System.Drawing.Color.DarkViolet;
+            this.BtnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnUpdate.Location = new System.Drawing.Point(451, 406);
+            this.BtnUpdate.Name = "BtnUpdate";
+            this.BtnUpdate.Size = new System.Drawing.Size(133, 40);
+            this.BtnUpdate.TabIndex = 7;
+            this.BtnUpdate.Text = "Update";
+            this.BtnUpdate.UseVisualStyleBackColor = false;
+            this.BtnUpdate.Click += new System.EventHandler(this.BtnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Silver;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.btnDelete.Location = new System.Drawing.Point(642, 406);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(133, 40);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Silver;
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.btnCancel.Location = new System.Drawing.Point(836, 406);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(133, 40);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Save
+            // 
+            this.Save.BackColor = System.Drawing.Color.Lime;
+            this.Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.Save.Location = new System.Drawing.Point(439, 406);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(145, 42);
+            this.Save.TabIndex = 11;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = false;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // pictureBoxQRCode
+            // 
+            this.pictureBoxQRCode.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxQRCode.Location = new System.Drawing.Point(182, 205);
+            this.pictureBoxQRCode.Name = "pictureBoxQRCode";
+            this.pictureBoxQRCode.Size = new System.Drawing.Size(184, 180);
+            this.pictureBoxQRCode.TabIndex = 12;
+            this.pictureBoxQRCode.TabStop = false;
+            // 
+            // StudentID
+            // 
+            this.StudentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StudentID.Location = new System.Drawing.Point(225, 24);
+            this.StudentID.Multiline = true;
+            this.StudentID.Name = "StudentID";
+            this.StudentID.Size = new System.Drawing.Size(170, 32);
+            this.StudentID.TabIndex = 13;
+            this.StudentID.TextChanged += new System.EventHandler(this.StudentID_TextChanged_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(104, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Student ID:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1069, 573);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.StudentID);
+            this.Controls.Add(this.pictureBoxQRCode);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.BtnUpdate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.isSingleCheckBox);
+            this.Controls.Add(this.StudentAge);
+            this.Controls.Add(this.StudentName);
+            this.Controls.Add(this.StudentsGrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.StudentsGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxQRCode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView StudentsGrid;
+        private System.Windows.Forms.TextBox StudentName;
+        private System.Windows.Forms.TextBox StudentAge;
+        private System.Windows.Forms.CheckBox isSingleCheckBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BtnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.PictureBox pictureBoxQRCode;
+        private System.Windows.Forms.TextBox StudentID;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DataGridAddStudent/Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Zen.Barcode;
+
+
+
+namespace DataGridAddStudent
+{
+    public partial class Form1 : Form
+    {
+
+        // create a connetion 
+        MySqlConnection connection = new MySqlConnection("Server=localhost;Database=bscs3db;User=root;Password='';");
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            try
+            {
+                connection.Open();
+                string query = "SELECT * FROM studentstbl ORDER by StudentID DESC ";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query, connection);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                StudentsGrid.DataSource = dt;
+                StudentsGrid.CellClick += DataGridView1_CellClick;
+
+                // Set StudentID TextBox as read-only
+                StudentID.ReadOnly = true;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Hoy may error : " + ex.Message);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                StudentsGrid.Rows[e.RowIndex].Selected = true;
+
+                DataGridViewRow clickedRow = StudentsGrid.Rows[e.RowIndex];
+                string rowDataStudentid = clickedRow.Cells["Studentid"].Value.ToString();
+                string Name = clickedRow.Cells["Name"].Value.ToString();
+                string Age = clickedRow.Cells["Age"].Value.ToString();
+                var IsSingle = clickedRow.Cells["IsSingle"].Value;
+
+                // Set the values to the corresponding controls
+                StudentID.Text = rowDataStudentid;
+                StudentName.Text = Name;
+                StudentAge.Text = Age;
+                isSingleCheckBox.Checked = Convert.ToBoolean(IsSingle);
+
+                // Display the QR code for the current student
+                GenerateQRCode(rowDataStudentid, pictureBoxQRCode);
+
+                Save.Visible = false;
+                BtnUpdate.Visible = true;
+                btnDelete.Enabled = true;
+
+
+
+            }
+            else
+            {
+                // No row selected, reset controls and toggle button visibility
+                StudentID.Text = "";
+                StudentName.Text = "";
+                StudentAge.Text = "";
+                isSingleCheckBox.Checked = false;
+                pictureBoxQRCode.Image = null;
+
+            }
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "INSERT INTO studentstbl (Name, Age, isSingle) VALUES (@name, @Age, @isSingle)";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                // Set parameters
+                cmd.Parameters.AddWithValue("@name", StudentName.Text);
+                cmd.Parameters.AddWithValue("@Age", int.Parse(StudentAge.Text));
+                cmd.Parameters.AddWithValue("@isSingle", isSingleCheckBox.Checked);
+
+                // Execute the query
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Data inserted successfully.");
+
+                StudentsGrid.DataSource = null;
+                string query2 = "SELECT * FROM studentstbl ORDER by StudentID DESC ";
+                MySqlDataAdapter adapter = new MySqlDataAdapter(query2, connection);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                StudentsGrid.DataSource = dt;
+
+                StudentID.Text = "";
+                StudentName.Text = "";
+                StudentAge.Text = "";
+                isSingleCheckBox.Checked = false;
+
+                Save.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hoyy hndi naka insert!!!" + ex.Message);
+            }
+
+        }
+
+        private void StudentID_TextChanged_1(object sender, EventArgs e)
+        {
+            if (!(string.IsNullOrEmpty(StudentName.Text)) && !(string.IsNullOrEmpty(StudentAge.Text)))
+            {
+                Save.Enabled = true;
+                BtnUpdate.Visible = true;
+                btnDelete.Enabled = true;
+
+
+            }
+            else
+            {
+                Save.Enabled = false;
+                BtnUpdate.Visible = false;
+                btnDelete.Enabled = false;
+
+            }
+        }
+
+        private void StudentName_TextChanged(object sender, EventArgs e)
+        {
+            if (!(string.IsNullOrEmpty(StudentName.Text)) && !(string.IsNullOrEmpty(StudentAge.Text)))
+            {
+                Save.Visible = false;
+                Save.Enabled = false;
+                btnDelete.Enabled = true;
+            }
+            else
+            {
+                Save.Visible = true;
+                Save.Enabled = true;
+                btnDelete.Enabled = false;
+            }
+        }
+
+        private void StudentAge_TextChanged(object sender, EventArgs e)
+        {
+            if (!(string.IsNullOrEmpty(StudentName.Text)) && !(string.IsNullOrEmpty(StudentAge.Text)))
+            {
+                Save.Enabled = true; // Enable Submitbtn when both textboxes have data
+                BtnUpdate.Visible = true;
+                btnDelete.Enabled = false;
+            }
+            else
+            {
+                Save.Enabled = false; // Disable Submitbtn if any of the textboxes is empty
+                BtnUpdate.Visible = false;
+                btnDelete.Enabled = false;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            StudentID.Text = "";
+            StudentName.Text = "";
+            StudentAge.Text = "";
+            isSingleCheckBox.Checked = false;
+            pictureBoxQRCode.Image = null;
+
+            Save.Visible = true;
+            Save.Enabled = false;
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to delete the selected student(s)?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                if (StudentsGrid.SelectedRows.Count > 0)
+                {
+                    foreach (DataGridViewRow row in StudentsGrid.SelectedRows)
+                    {
+                        int studentId = Convert.ToInt32(row.Cells["Studentid"].Value); // Replace "Studentid" with the actual column name
+
+                        // Assuming you have a method to delete the record from the database
+                        if (DeleteStudent(studentId))
+                        {
+                            // Remove the row from the DataGridView
+                            StudentsGrid.Rows.Remove(row);
+
+                            // Clear the textboxes
+                            StudentID.Text = "";
+                            StudentName.Text = "";
+                            StudentAge.Text = "";
+                            isSingleCheckBox.Checked = false;
+
+                            Save.Visible = true;
+
+
+
+                            MessageBox.Show("Student with ID " + studentId + " has been deleted successfully.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error deleting student with ID: " + studentId);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a row to delete.");
+                }
+            }
+
+        }
+
+        // Replace this method with your actual logic to delete a student from the database
+        private bool DeleteStudent(int studentId)
+        {
+            try
+            {
+                using (MySqlCommand deleteCmd = new MySqlCommand("DELETE FROM Studentstbl WHERE Studentid = @Studentid", connection))
+                {
+                    deleteCmd.Parameters.AddWithValue("@Studentid", studentId);
+                    deleteCmd.ExecuteNonQuery();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Log the error or handle it as needed
+                Console.WriteLine("Error deleting student: " + ex.Message);
+                return false;
+            }
+        }
+
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to update the selected student?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                if (StudentsGrid.SelectedRows.Count > 0)
+                {
+                    DataGridViewRow selectedRow = StudentsGrid.SelectedRows[0];
+
+                    // Get the student ID from the selected row
+                    int studentId = Convert.ToInt32(selectedRow.Cells["Studentid"].Value);
+
+                    try
+                    {
+                        // Open a new connection for the update operation
+                        using (MySqlConnection updateConnection = new MySqlConnection("Server=localhost;Database=bscs3db;User=root;Password='';"))
+                        {
+                            updateConnection.Open();
+
+                            // Update query
+                            string updateQuery = "UPDATE Studentstbl SET Name = @Name, Age = @Age, IsSingle = @IsSingle WHERE Studentid = @Studentid";
+                            MySqlCommand updateCmd = new MySqlCommand(updateQuery, updateConnection);
+
+                            // Set parameters
+                            updateCmd.Parameters.AddWithValue("@Name", StudentName.Text);
+                            updateCmd.Parameters.AddWithValue("@Age", int.Parse(StudentAge.Text));
+                            updateCmd.Parameters.AddWithValue("@IsSingle", isSingleCheckBox.Checked);
+                            updateCmd.Parameters.AddWithValue("@Studentid", studentId);
+
+                            // Execute the update query
+                            updateCmd.ExecuteNonQuery();
+
+                            // Refresh the DataGridView
+                            StudentsGrid.DataSource = null;
+                            string selectQuery = "SELECT * FROM studentstbl ORDER BY Studentid DESC";
+                            MySqlDataAdapter adapter = new MySqlDataAdapter(selectQuery, updateConnection);
+                            DataTable dt = new DataTable();
+                            adapter.Fill(dt);
+                            StudentsGrid.DataSource = dt;
+
+                            // Clear the textboxes
+                            StudentID.Text = "";
+                            StudentName.Text = "";
+                            StudentAge.Text = "";
+                            isSingleCheckBox.Checked = false;
+
+                            Save.Visible = true;
+
+
+                            MessageBox.Show("Data Updated Successfully.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error updating student: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select a row to update.");
+                }
+            }
+        }
+
+        private void GenerateQRCode(int studentID)
+        {
+            Zen.Barcode.CodeQrBarcodeDraw qrcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            pictureBoxQRCode.Image = qrcode.Draw(studentID.ToString(), 50);
+        }
+
+
+        private void GenerateQRCode(string data, PictureBox pictureBox)
+        {
+            try
+            {
+                // Use the Zen Barcode Framework to create a QR code
+                Zen.Barcode.CodeQrBarcodeDraw qrCode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+
+                // Adjust the size of the QR code based on the PictureBox dimensions
+                int pictureBoxSize = Math.Min(pictureBox.Width, pictureBox.Height);
+
+                // Draw the QR code and set it as the PictureBox's image
+                pictureBox.Image = qrCode.Draw(data, pictureBoxSize);
+
+                pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox.Image = pictureBox.Image;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error generating QR code: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                {
+                    if (!string.IsNullOrEmpty(StudentID.Text))
+                    {
+                        // Assuming the student ID is stored in studentidtextbox.Text
+                        int studentId = Convert.ToInt32(StudentName.Text);
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid QR code. Please select a student first.");
+                    }
+
+                }
+            }
+        }
+    }
+}
